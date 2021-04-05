41ec91ab1dc0fd901daeea19f22631dc110174d9
Akshay Kumar <work.akshay.kumar@gmail.com>
2021-04-05 21:36:27 +0530

    Add jsdoc tags for responseHandler utils

--- response-handler.js
+++ response-handler.js
@@ -1,4 +1,4 @@
-const defaultStatusCodes = {
+let defaultStatusCodes = {
   400: 'INCORRECT_REQUEST_FORMAT',
   401: 'INVALID_SESSION',
   403: 'FORBIDDEN',
@@ -6,8 +6,34 @@ const defaultStatusCodes = {
   408: 'REQUEST_TIMED_OUT',
 }
 
-const responseHandler = (response, statusCodes = defaultStatusCodes) => {
+/**
+ * Configure status code based errors for all responseHandlers
+ * @param {object} statusCodes
+ */
+const configureStatusCodeBasedErrors = (statusCodes = {}) => {
+  defaultStatusCodes = {
+    ...defaultStatusCodes,
+    ...statusCodes,
+  }
+}
+
+/**
+ * Create an responseHandler with statusCodes (optional, will merge)
+ * @param {object} statusCodes
+ * @returns {function} errorHandler
+ */
+const buildResponseHandler = (statusCodes = {}) => (response) => {
+  const requestStatusCodes = {
+    ...defaultStatusCodes,
+    ...statusCodes,
+  }
   if (response?.ok) return response.json()
-  if (response && response.status && statusCodes[response.status]) throw Error(statusCodes[response.status])
+  if (response && response.status && requestStatusCodes[response.status])
+    throw Error(requestStatusCodes[response.status])
   throw Error('REQUEST_TIMED_OUT') // This will be reached only if the request was aborted using abort controller
+}
+
+module.exports = {
+  buildResponseHandler,
+  configureStatusCodeBasedErrors,
 }
